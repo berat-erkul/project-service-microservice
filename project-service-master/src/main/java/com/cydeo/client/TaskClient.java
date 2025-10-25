e8cc2e4bc42d3443d3db974205881f9a3a5f6cd1
berat-erkul <beraterkul00@gmail.com>
2025-10-25 20:11:33 +0300

    adds eureka-client (with exclusions), jersey and actuator dependencies

--- project-service-master/src/main/java/com/cydeo/client/TaskClient.java
+++ project-service-master/src/main/java/com/cydeo/client/TaskClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
-@FeignClient(name = "project-service", url = "http://localhost:8383/api/v1/task")
+@FeignClient(name = "task-service", url = "http://localhost:8383/api/v1/task")
 public interface TaskClient {
 
     @GetMapping("/count/project/{projectCode}")
